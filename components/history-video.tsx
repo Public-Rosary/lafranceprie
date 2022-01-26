8a5484d10c8ea7121a1c8531d5e407ce76dbb3d4
La France prie <opensource@lafranceprie.org>
2022-01-26 21:09:10 +0100

    remove video from homepage

--- components/history-video.tsx
+++ components/history-video.tsx
@@ -2,8 +2,8 @@ import type { NextPage } from "next";
 
 const HistoryVideo: NextPage = () => {
   return (
-    <div className="mt-8 flex flex-wrap place-content-center">
-      <div className="max-w-[560px] p-4 text-gray-500 text-lg">
+    <div className="flex flex-wrap place-content-center">
+      <div className="container p-4 max-w-4xl text-gray-500 text-lg">
         <p className="p-4">
           L&apos;initiative &ldquo;La France Prie&ldquo; reprend en France
           une initiative lancée en Autriche le 29 novembre 2021 et qui
@@ -18,21 +18,6 @@ const HistoryVideo: NextPage = () => {
           chez elle pour ainsi multiplier le nombre de lieu de récitation du chapelet
           dans l&apos;espace public.
         </p>
-      </div>
-
-      <div className="text-center text-gray-500">
-        <iframe
-          className="w-full aspect-video max-w-[560px] max-h-[315px]"
-          width="560"
-          height="315"
-          src="https://www.youtube-nocookie.com/embed/bXNH4nr4q-g"
-          title="YouTube video player"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-          loading="lazy"
-        ></iframe>
-        Vidéo de présentation par Louis-Pierre, initiateur
       </div>
     </div>
   );
